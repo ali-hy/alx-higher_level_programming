13bc3c2084448fd87e6915c21f9c99859122b8b0
ali-hy <ali.haitham.ymy@gmail.com>
2024-01-11 01:16:03 +0200

    100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -6,8 +6,3 @@ def weight_average(my_list=[]):
         res += t[0] * t[1]
         total_weight += t[1]
     return res/total_weight
-
-my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
-# = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
-result = weight_average(my_list)
-print("Average: {:0.2f}".format(result))
