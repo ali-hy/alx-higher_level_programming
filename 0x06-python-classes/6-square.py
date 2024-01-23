7a3d48a7f600e99cb5ff42293a2d56b826f9a317
ali-hy <ali.haitham.ymy@gmail.com>
2024-01-23 23:44:31 +0200

    6-square.py

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Square"""
+
+
+class Square:
+    """Square class"""
+
+    def __init__(self, size=0, position=(0, 0)) -> None:
+        """init square"""
+        self.size = size
+        self.position = position
+
+    def area(self):
+        """returns area of square"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """access size property"""
+        return self.__size
+
+    @size.setter
+    def size(self, val):
+        """set size property"""
+        if type(val) is not int:
+            raise TypeError("size must be an integer")
+        elif val < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = val
+    
+    @property
+    def position(self):
+        """access position"""
+        return self.__position
+    
+    @position.setter
+    def position(self, val):
+        """set position"""
+        if len(val) != 2 or val[0] < 0 or  val[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = val
+
+    def my_print(self):
+        """print square"""
+        if self.size == 0:
+            print('')
+            return
+
+        print('\n'*self.position[1],end='')
+        for _ in range(self.size):
+            print(' '*self.position[0] + '#'*self.size)
