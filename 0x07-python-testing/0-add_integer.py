033ba98c5bbdc5bde56719b4694ba60ccbaa2732
ali-hy <ali.haitham.ymy@gmail.com>
2024-01-30 22:18:30 +0200

    0-add_integer.py ]

---
+++ 0x07-python-testing/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""add_integer"""
+
+
+def add_integer(a, b=98):
+    """add_integer - adds two integers"""
+    if not isinstance(a, (int, float)):
+        raise TypeError('a must be an integer')
+    if not isinstance(b, (int, float)):
+        raise TypeError('b must be an integer')
+    return int(a) + int(b)
