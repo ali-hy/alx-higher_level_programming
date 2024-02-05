db4968f456e903e4f84907e76c0975f8fabf7bfe
ali-hy <ali.haitham.ymy@gmail.com>
2024-02-05 16:01:58 +0200

    no init

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,8 +4,8 @@
 
 class MyList(list):
     '''MyList'''
-    def __init__(self):
-        super().__init__()
+    #  def __init__(self):
+    #     super().__init__()
 
     def print_sorted(self):
         print(sorted(self))
