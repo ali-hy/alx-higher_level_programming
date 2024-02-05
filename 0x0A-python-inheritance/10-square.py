30e5afd77cd6b566da14d1a2c636129842faec1c
ali-hy <ali.haitham.ymy@gmail.com>
2024-02-05 20:12:29 +0200

    10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -42,7 +42,6 @@ class Square(Rectangle):
     def __init__(self, size):
         self.integer_validator('size', size)
         self.__size = size
-        super().__init__(size, size)
 
     def area(self):
-        return super().area()
+        return self.__size * self.__size
