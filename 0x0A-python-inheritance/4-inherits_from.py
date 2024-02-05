453b781fae94cd858ad82359d4b35081f4efe34c
ali-hy <ali.haitham.ymy@gmail.com>
2024-02-05 19:14:06 +0200

    4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,6 +4,6 @@
 
 def inherits_from(obj, a_class):
     '''check if obj inherits from a_class'''
-    if isinstance(obj, a_class):
+    if obj.__class__ == a_class:
         return False
     return issubclass(obj.__class__, a_class)
