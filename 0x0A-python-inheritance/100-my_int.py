1e8b902ce235ff7ce2efc590621b5e77b7a5bcf9
ali-hy <ali.haitham.ymy@gmail.com>
2024-02-05 20:36:20 +0200

    100-my_int.py

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''my rebelious int'''
+
+class MyInt(int):
+    def __eq__(self, __value: object) -> bool:
+        return super().__ne__(__value)
+
+    def __ne__(self, __value: object) -> bool:
+        return super().__eq__(__value)
