82a05bd4a61ddbdeb941077786124a47a9feff6a
ali-hy <ali.haitham.ymy@gmail.com>
2024-02-06 12:11:19 +0200

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import json
+from sys import argv
+from os import path
+'''read file'''
+
+class Student:
+    '''student'''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        serializable = {}
+        for attr, value in self.__dict__.items():
+            serializable[attr] = value
+
+        return serializable
+
+s = Student('ali', 'haitham', 19)
