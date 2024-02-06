d53c37558de3cd191508dcf41fcfcf2995904be3
ali-hy <ali.haitham.ymy@gmail.com>
2024-02-06 14:01:23 +0200

    0-

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,9 +4,6 @@
 
 def read_file(filename=""):
     '''read all the entire file with the name <filename>'''
-    res = ''
     with open(filename, 'r', encoding='utf-8') as f:
-        res = f.read()
-    print(res)
-
-read_file('0-test.txt')
+        for line in f:
+            print(line, end='')
