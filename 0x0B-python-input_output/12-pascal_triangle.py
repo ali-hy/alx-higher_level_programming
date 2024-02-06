ff997e3186e5d0bc1d9f2894a0146b524cb3cfa8
ali-hy <ali.haitham.ymy@gmail.com>
2024-02-06 13:47:01 +0200

    12-pascal_triangle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''pascal triangle'''
+
+
+def pascal_triangle(n):
+    '''returns list representing pascal's triangle at level n'''
+    if (n <= 0):
+        return []
+
+    res = [1]
+    n -= 1
+
+    while n > 0:
+        new_res = list(res)
+        for i in range(1, len(new_res)):
+            new_res[i] = res[i] + res[i-1]
+        new_res.append(1)
+        res = new_res
+        n -= 1
+
+    return res
+
+for i in range(0, 10):
+    print(pascal_triangle(i))
