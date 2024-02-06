c2f368b744316aa6b5f586883407b54f46dfd82b
ali-hy <ali.haitham.ymy@gmail.com>
2024-02-06 14:00:05 +0200

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,7 +6,7 @@ def read_file(filename=""):
     '''read all the entire file with the name <filename>'''
     res = ''
     with open(filename, 'r', encoding='utf-8') as f:
-        res = f.read().rstrip()
+        res = f.read()
     print(res)
 
 read_file('0-test.txt')
