696818762db53139747f10a22f02d25b2805cbe1
ali-hy <ali.haitham.ymy@gmail.com>
2024-02-06 11:14:06 +0200

    2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''read file'''
+
+def append_write(filename="", text=""):
+    '''append <text> to file called <filename>'''
+    res = 0
+    with open(filename, 'a', encoding='utf-8') as f:
+        res = f.write(text)
+    return res
