b0822bf2faccf40d1bc73ad61bc086d534dc8393
ali-hy <ali.haitham.ymy@gmail.com>
2024-03-13 23:07:15 +0200

    7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,6 +3,7 @@ USE hbtn_0d_usa;
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    state_id INT NOT NULL REFERENCES states (id)
+    state_id INT NOT NULL REFERENCES states (id),
+    name VARCHAR(256)
 );
 
