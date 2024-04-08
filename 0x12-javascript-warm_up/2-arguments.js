fe214a291d7ce6b6a41c472cab328c7be9c5009c
ali-hy <ali.haitham.ymy@gmail.com>
2024-04-08 13:56:16 +0200

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const process = require('process');
 
-if (process.argv[2]) {
+if (process.argv.length > 2) {
   console.log('Argument found');
 } else {
   console.log('No argument');
