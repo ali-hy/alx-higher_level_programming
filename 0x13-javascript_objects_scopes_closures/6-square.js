c7b41c4de94e8e4ee3d8ba61de01213814835fa4
ali-hy <ali.haitham.ymy@gmail.com>
2024-04-09 14:54:16 +0200

    6-

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,51 +1,8 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    if (w === undefined || h === undefined || w <= 0 || h <= 0) { return; }
-    this.width = w;
-    this.height = h;
-  }
+const Square = require("./5-square");
 
-  charPrint (c = 'X') {
-    let res = '';
-    for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) {
-        res += c;
-      }
-      if (i < this.height - 1) {
-        res += '\n';
-      }
-    }
-    console.log(res);
-  }
-
-  print () {
-    let res = '';
-    for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) {
-        res += 'X';
-      }
-      if (i < this.height - 1) {
-        res += '\n';
-      }
-    }
-    console.log(res);
-  }
-
-  rotate () {
-    const temp = this.width;
-    this.width = this.height;
-    this.height = temp;
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
-}
-
-module.exports = class Square extends Rectangle {
+module.exports = class extends Square {
   constructor (size) {
     super(size, size);
   }
