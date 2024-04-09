eed50e209f452421f8c7ade6ca8922aad40883f4
ali-hy <ali.haitham.ymy@gmail.com>
2024-04-09 15:53:50 +0200

    101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,10 +2,9 @@
 
 const dict = require('./101-data').dict;
 
-const res = {}
+const res = {};
 Object.entries(dict).forEach((e) => {
-    if (!(e[1] in res))
-        res[e[1]] = [];
-    res[e[1]].push(e[0]);
-})
+  if (!(e[1] in res)) { res[e[1]] = []; }
+  res[e[1]].push(e[0]);
+});
 console.log(res);
