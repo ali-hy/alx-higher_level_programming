9ab81659c277b100d82524958eb4284176bfeaf1
ali-hy <ali.haitham.ymy@gmail.com>
2024-04-09 14:06:12 +0200

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -8,14 +8,10 @@ class Rectangle {
   }
 
   print () {
-    this.charPrint();
-  }
-
-  charPrint (c = 'X') {
     let res = '';
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
-        res += c;
+        res += 'X';
       }
       if (i < this.height - 1) {
         res += '\n';
@@ -39,6 +35,19 @@ class Rectangle {
 class Square extends Rectangle {
   constructor (size) {
     super(size, size);
+  }
+
+  charPrint (c='X') {
+    let res = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        res += c;
+      }
+      if (i < this.height - 1) {
+        res += '\n';
+      }
+    }
+    console.log(res);
   }
 }
 
