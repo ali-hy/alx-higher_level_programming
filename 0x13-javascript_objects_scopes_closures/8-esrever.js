28e79ca0db5901aef3d84105d602bbcb65b2bebf
ali-hy <ali.haitham.ymy@gmail.com>
2024-04-09 15:03:27 +0200

    8-esrever.js

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+module.exports.esrever = function (list) {
+  const res = new Array(list.length);
+  for (let i = 0; i < res.length; i++) {
+    res[i] = list[res.length - (i + 1)]
+  }
+};
