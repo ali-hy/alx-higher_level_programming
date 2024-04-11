616f7c84a04e1c3091e25c633346092cc167fe5a
ali-hy <ali.haitham.ymy@gmail.com>
2024-04-11 08:59:19 +0200

    1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,11 +12,12 @@ if __name__ == '__main__':
     )
 
     cur = db.cursor()
-    cur.execute('SELECT * FROM states '
-                + 'WHERE states.name '
-                + 'LIKE "N%" ORDER BY states.id;')
+    cur.execute('''SELECT * FROM states
+                WHERE states.name LIKE "N%"
+                ORDER BY states.id;''')
 
     if not cur.rowcount:
+        db.close()
         exit(1)
 
     for _ in range(cur.rowcount):
