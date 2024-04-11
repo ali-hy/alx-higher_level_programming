2255579d4b4b24351df5df1a4e6542c157e70843
ali-hy <ali.haitham.ymy@gmail.com>
2024-04-11 09:46:41 +0200

    10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -14,7 +14,8 @@ if __name__ == "__main__":
     Session = sessionmaker(engine)
     session = Session()
 
-    res = session.query(State.id).filter(State.name == sys.argv[4]).one_or_none()
+    res = session.query(State.id)\
+        .filter(State.name == sys.argv[4]).one_or_none()
     print(res[0] if res is not None else 'Not found')
 
     session.close()
