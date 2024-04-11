ce407d17458eb74b4db8aa4f0c62743ef0ddb1e1
ali-hy <ali.haitham.ymy@gmail.com>
2024-04-11 12:34:07 +0200

    101-relationship_states_cities_list.

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Start link class to table in database
+"""
+script that lists all State objects, and corresponding
+City objects, contained in the database hbtn_0e_101_usa
 """
 import sys
 from relationship_state import Base, State
