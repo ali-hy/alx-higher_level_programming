057786a20471146273601b3c4997a84da9466f9f
ali-hy <ali.haitham.ymy@gmail.com>
2024-04-11 09:16:29 +0200

    why not work

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -15,7 +15,8 @@ if __name__ == '__main__':
                 WHERE states.name = '{}'
                 ORDER BY states.id;'''.format(argv[4]))
 
-    for _ in range(cur.rowcount):
-        print(cur.fetchone())
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
 
     db.close()
