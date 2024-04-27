3e1a2be0ea992487a8bddc4fd671c3e55295f5e6
ali-hy <ali.haitham.ymy@gmail.com>
2024-04-27 12:00:45 +0300

    6-peak.

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+''' find the peak of an unsorted array of integers '''
+
+
+def find_peak(list_of_integers):
+    if not list_of_integers:
+        return None
+    low = 0
+    high = len(list_of_integers) - 1
+    while low < high:
+        mid = (low + high) // 2
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+    return list_of_integers[low]
