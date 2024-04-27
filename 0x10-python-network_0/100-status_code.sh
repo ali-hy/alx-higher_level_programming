29da452f8bb63ccea6c0349490a22ee0535f15c8
ali-hy <ali.haitham.ymy@gmail.com>
2024-04-27 12:40:27 +0300

    100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # This script sends a POST request to the URL passed as the first argument
-response=$(curl -s -o /dev/null -w "%{http_code}" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1")
-echo "Status code: $response"
+curl -s -o /dev/null -w "%{http_code}" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
