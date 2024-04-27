28e4b2fbec3a32824780d809e63bd41077d5a4fc
ali-hy <ali.haitham.ymy@gmail.com>
2024-04-27 12:17:45 +0300

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 '''Fetches https://intranet.hbtn.io/status'''
-import urllib.request
+from urllib import request
 
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
+    with request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
