8a25ca417dc07a3689e3818fb323c63100e8744a
ali-hy <ali.haitham.ymy@gmail.com>
2024-04-27 11:54:28 +0300

    5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script sends a POST request to the URL passed as the first argument, includes variables email and subject in the request body, and displays the body of the response
+# This script sends a POST request to the URL passed as the first argument
 curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
