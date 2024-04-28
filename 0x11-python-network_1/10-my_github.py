9a049f2f08753428aaea1919b28a6d08ddc231f2
ali-hy <ali.haitham.ymy@gmail.com>
2024-04-28 13:59:03 +0300

    10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''This script takes your GitHub credentials (username and password) and uses the GitHub API to display your id'''
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://api.github.com/users/{}".format(sys.argv[1])
+    password = sys.argv[2]
+    response = requests.get(url, headers={'Authorization': 'Bearer {}'.format(password)})
+    print(response.json().get('id'))
