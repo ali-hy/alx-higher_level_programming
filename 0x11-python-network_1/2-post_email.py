cfc96edcf2f88eacc1c57f5cbcc4c37bbc998fc0
ali-hy <ali.haitham.ymy@gmail.com>
2024-04-28 11:03:41 +0300

    2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import sys
+import urllib.request
+import urllib.parse
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    req = urllib.request.Request(url, data)
+
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
